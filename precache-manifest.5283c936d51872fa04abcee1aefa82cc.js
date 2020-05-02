80c93ec7316110282e8af9be39d8423a19b9df8a
gajendra20a <gajendra18862@gmail.com>
2020-05-02 20:54:44 +0530

    Updates

---
+++ precache-manifest.5283c936d51872fa04abcee1aefa82cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84754ab26b562e5df56e84e186c55980",
+    "url": "/parking-lot-system/index.html"
+  },
+  {
+    "revision": "9ec71c6f6d9c60b61f5f",
+    "url": "/parking-lot-system/static/css/main.3f58b5b1.chunk.css"
+  },
+  {
+    "revision": "a98f71a2c7649fe48cc5",
+    "url": "/parking-lot-system/static/js/2.ee4944e2.chunk.js"
+  },
+  {
+    "revision": "e9bf69872b63633e0c0eda508c130376",
+    "url": "/parking-lot-system/static/js/2.ee4944e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec71c6f6d9c60b61f5f",
+    "url": "/parking-lot-system/static/js/main.d30ad3ed.chunk.js"
+  },
+  {
+    "revision": "836e680a83a73554efb7",
+    "url": "/parking-lot-system/static/js/runtime-main.505dc7a2.js"
+  }
+]);
